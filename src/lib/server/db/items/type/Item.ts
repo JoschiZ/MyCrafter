c9cc147a14ade0a5e51c7f47382586cba10bcb05
Joschi <joschi.grey@posteo.de>
2023-01-07 14:10:58 +0100

    Added items type, scheme and mongo export

---
+++ src/lib/server/db/items/type/Item.ts
@@ -0,0 +1,29 @@
+export type Item = {
+    type: string
+    name: string
+    id: number
+    subclassID: number
+    subType: string
+    classID: number
+    tradeSkillLineID: number
+    wowCat: string
+    inventoryType: string
+    recipe: Recipe
+}
+
+export type Recipe = {
+    recipeID: number,
+    reagents?: Reagent[]
+    modifiedSlots?: ModifiedSlot[]
+}
+
+export type Reagent = {
+    quantity: number,
+    name: string,
+    id: number
+}
+
+export type ModifiedSlot = {
+    name: string,
+    id: number
+}
