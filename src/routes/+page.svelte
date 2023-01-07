0aa3bf726669a398103175b081c5f5584b0eb0af
Joschi <joschi.grey@posteo.de>
2023-01-07 15:29:10 +0100

    Fixed profession-button hover to be correctly displayed instead of bigger on the bottom

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,15 +8,13 @@
 	<title>MyCrafter.io - Main Page</title>
 </svelte:head>
 
-<section class="dp01 profession-button-holder">
+<section class="dp02 profession-button-holder">
 	<h1>What are you looking for today?</h1>
 	<div class="profession-button-grid">
 		{#each professions as profession}
 			<div class="profession-button">
 				<a href="/professions/{profession.name}">
-					<picture>
-						<img alt={profession.name} src={profession.icon} />
-					</picture>
+					<img alt={profession.name} src={profession.icon} />
 				</a>
 			</div>
 		{/each}
@@ -30,8 +28,9 @@
 		align-items: center;
 		justify-content: center;
 		padding: 1em;
-		border-radius: 2%;
+		border-radius: 5px;
 		margin-top: 1em;
+		border: solid 1px white;
 	}
 
 	.profession-button-grid {
@@ -41,22 +40,25 @@
 	}
 
 	a {
+		display: flex;
 		text-decoration: none;
 	}
+
 	.profession-button {
-		width: fit-content;
-		height: fit-content;
 		padding: 0.5em;
 		border-radius: 24%;
 		margin: 0.25em;
 	}
+
 	.profession-button:hover {
 		background-color: hsla(0, 100%, 100%, 0.05);
-		box-shadow:
-		/* offset-x | offset-y | blur-radius | spread-radius | color */ 0px 1px 1px 0px
-				hsla(0, 0%, 0%, 0.14),
-			0px 2px 1px -1px hsla(0, 0%, 0%, 0.12), 0px 1px 3px 0px hsla(0, 0%, 0%, 0.2);
+	box-shadow:
+		/* offset-x | offset-y | blur-radius | spread-radius | color */
+		0px 1px 1px 0px hsla(0,0%,0%,0.14), 
+		0px 2px 1px -1px hsla(0,0%,0%,0.12), 
+		0px 1px 3px 0px hsla(0,0%,0%,0.2);
 	}
+
 	.profession-button img {
 		display: inline-block;
 		height: 5em;
